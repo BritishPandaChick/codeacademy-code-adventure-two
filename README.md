1369c1996c85e0f38d7a9a6d5d91d7be6528133a
BritishPandaChick <sbartley1080@gmail.com>
2022-03-16 15:23:13 -0400

    Fix README. Change title in index.html

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Codeacademy Code Your Own Adventure Game Part Two
-Code Your Own Adventure Game 2 project from Codeacademy JavaScript course. Made with HTML and JavaScript.
+# Codecademy Code Your Own Adventure Game Part Two
+Code Your Own Adventure Game 2 project from Codecademy JavaScript course.
 
 ## Instructions
 Clone or download zip file. Open index.html file in your browser.
 
 ## Contributing
-This repository is based on starter code from Codeacademy. Therefore I will not be accepting any pull requests.
+This repository is based on starter code from Codecademy. Therefore I will not be accepting any pull requests.
 
 ## Acknowledgements
-* Codeacademy
+* Codecademy
--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
 var user = prompt("What is your name?").toUpperCase();
 
 switch(user) {
-  case 'Jack':
-    var hungry = true;
-    var tired = true;
+    case 'Jack':
+        var hungry = true;
+        var tired = true;
 
-    if (hungry && tired) {
-      console.log("Hi Jack! Want something to eat?");
-    } else {
-      console.log("Hi Jack! How are you?");
-    }
-    //console.log("Hi Jack!");
-  break;
+        if (hungry && tired) {
+            console.log("Hi Jack! Want something to eat?");
+        } else {
+            console.log("Hi Jack! How are you?");
+        }
+    break;
+    
+    case 'Jill':
+        var bored = false;
+        var busy = true;
 
-  case 'Jill':
-    var bored = false;
-    var busy = true;
-
-    if (bored || busy) {
-      console.log("Hi Jill. Any plans today?");
-    } else {
-      console.log("Hi Jill. Are you busy?");
-    }
-    //console.log("Hi Jill!");
-  break;
-
-  case 'Sarah':
-    console.log("Hi Sarah!");
-  break;
-  default:
+        if (bored || busy) {
+            console.log("Hi Jill. Any plans today?");
+        } else {
+            console.log("Hi Jill. Are you busy?");
+        }
+    break;
+        
+    case 'Sarah':
+        console.log("Hi Sarah!");
+    break;
+    default:
     console.log("Seriously what is your name?");
 }
